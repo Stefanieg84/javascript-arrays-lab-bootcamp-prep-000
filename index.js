7cedb498b308762f272b4359f5a5ed256a50269f
Stefanieg84 <stefanieg84@gmail.com>
2018-08-22 19:08:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function destructivelyRemoveFirstKitten(){
 
 function appendKitten(name){
   kittens = [...kittens, name]
+  return kittens
 }
 
-function 
+function prependKitten(name){
+  kittens = [name, ...kittens]
+  return kittens
+}
